8bdaadf3bbb965e76d21a585aefdab562e84431f
GalaxyXL <galaxyxl@live.com>
2018-05-24 20:55:59 +0800

    First time commit

---
+++ src/com/zmud/jlu/CommonContent.java
@@ -0,0 +1,10 @@
+package com.zmud.jlu;
+
+
+public interface CommonContent {
+	String CANNOTMOVE = "这个方向没有出路。";
+	enum DIRECTION {
+		EAST, WEST, SOUTH, NORTH, NORTHEAST, NORTHWEST, SOUTHEAST, SOUTHWEST,UP, DOWN 
+	};
+	int CHARS_PER_LINE = 40;
+}
